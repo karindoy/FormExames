18961db1b60ed43f3d694587479d07a95050b973
karin <doykarin@gmail.com>
2018-10-03 09:08:07 -0300

    sub

---
+++ index.js
@@ -0,0 +1,20 @@
+
+var express = require('express');
+var index = express();
+//var path = require("path");
+//var fs = require("fs");
+
+
+//var http = require('http').Server(index);
+
+//index.use('/libraries', express.static('node_modules'));
+//index.use(express.static(__dirname + '/public'));
+
+index.get('/', function (req, res) {
+  res.sendFile(__dirname + '/formulario.html');
+});
+
+index.listen(3000, function () {
+  console.log('Example index listening on port 3000!');
+});
+
